68a968ea98ae8642beb7cfd487a6da28ef9a6ee1
caothuyan15112004@gmail.com <113528064+atcact@users.noreply.github.com>
2023-06-04 23:46:21 +0700

    submit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Text_summarization_web_app
+Introduction
+
+This web application utilizes TF-IDF and TextRank algorithms to generate summaries for input text. It provides an efficient way to condense lengthy articles, documents, or any text into concise summaries.
+
+Technical Overview
+
+The Text Summarization Web App is built using the following technologies and libraries:
+
+Python: The backend is developed in Python programming language.
+Flask: A lightweight web framework is used to create the web application.
+HTML/CSS: The frontend interface is designed using HTML for structure and CSS for styling.
+TF-IDF Algorithm: Term Frequency-Inverse Document Frequency is used to calculate the importance of words in the text.
+TextRank Algorithm: A graph-based ranking algorithm is employed to identify the most relevant sentences for summarization.
+
+How to Install
+
+To install and run the Text Summarization Web App, follow these steps:
+
+1. Clone the repository
+2. Navigate to the project directory
+3. Install the required Python libraries using pip
+4. Run the Flask server
+5. Open a web browser and visit http://localhost:5000 to access the app
--- main/summarizer.py
+++ main/summarizer.py
@@ -34,6 +34,7 @@ class Summarizer:
         self.doc = doc
         self.shorten_num = 0
         self.sent_list = []
+        self.static_sent_list = []
         self.run()
 
 
@@ -81,8 +82,13 @@ class Summarizer:
         sent_list = []
         for id, sent in enumerate(list):
             new_sent = ""
+            new_static_sent = ""
             sent = sent.split()
             for word in sent:
+                if new_static_sent == "":
+                    new_static_sent = word
+                else:
+                    new_static_sent += " " + word
                 stemmed = self.stem_and_stop(word)
                 if not stemmed == "":
                     if id not in self.ids_to_max_counts:
@@ -101,13 +107,13 @@ class Summarizer:
                     else:
                         new_sent += " " + stemmed
             
+            if not new_static_sent == "":
+                self.static_sent_list.append(new_static_sent)
 
             if not new_sent == "":
                 sent_list.append(new_sent)
         return sent_list
                 
-
-
 
     def compute_tf(self):
         """
@@ -173,24 +179,10 @@ class Summarizer:
     def result(self):
         nx_graph = nx.from_numpy_array(self.cosine_matrix())
         scores = nx.pagerank(nx_graph)  
-        ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(self.sent_list)), reverse=True)
+        ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(self.static_sent_list)), reverse=True)
         
         # Generate summary
         summary = ""
         for i in range(self.shorten_num):
             summary += f"{ranked_sentences[i][1]}. "
         return summary
-
-# text = """Being healthy and fit in simple terms means taking good care of the body. We should remember that a healthy mind resides only in a healthy body. Good health of both mind and body helps one maintain the required energy level to achieve success in life. All of us must strive to achieve wholesome health.
-
-# Protecting your body from the intake of harmful substances, doing regular exercises, having proper food and sleep are some of the important instances that define a healthy lifestyle. Being fit allows us to perform our activities without being lethargic, restless or tired.
-
-# A healthy and fit person is capable of living the life to the fullest, without any major medical or physical issues. Being healthy is not only related to the physical well-being of a person, it also involves the mental stability or the internal peace of a person.
-
-# Generally, a healthy diet consists of taking a proper and healthy food which includes eating green and fresh vegetables, fruits, having milk, eggs, minerals, proteins and vitamins essential for a human’s lifestyle. Practicing Yoga including regular exercises in your daily routine also help you maintain your desired fitness, blood sugar and immunity level.
-
-# Healthy habits improve your physical appearance, mental stability, ability to perform activities in a better way, which help you lead a stress-free lifestyle, maintaining happy moods, high energy levels, etc. Each individual should take of one’s health on a priority; no single day should be skipped for making efforts on maintaining physical and mental fitness. Being happy is directly related to boosting your mental strength and health, so happiness can be considered as the result as well as the part of a healthy and fit lifestyle.
-
-# """
-# index = Summarizer(text)
-# print(index.result())
